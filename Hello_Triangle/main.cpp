3c4e17ba5162db14aade9e2a65577c1e6a684514
cugxy <cugxy@qq.com>
2018-03-06 10:46:29 +0800

    a

--- Hello_Triangle/main.cpp
+++ Hello_Triangle/main.cpp
@@ -15,7 +15,7 @@ const char* vertexShaderSource = "#version 330 core\n"
 "void main()\n"
 "{\n"
 "	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"    vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+"   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
